63c73ab11e7215027b85ff62cbc461d6824be67a
Matheus Costa <contato.matheusccp5@gmail.com>
2025-03-10 23:44:07 -0300

    feat: add homepage

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export", // Gera um site estático
+  images: {
+    unoptimized: true, // Evita problemas com otimização de imagens no GitHub Pages
+  },
+  basePath: isProd ? "/desafio3-ecommerce-compass-uol" : "", // Caminho base do repositório
+  assetPrefix: isProd ? "/desafio3-ecommerce-compass-uol/" : "", // Prefixo dos assets
 };
 
-export default nextConfig;
+module.exports = nextConfig;
